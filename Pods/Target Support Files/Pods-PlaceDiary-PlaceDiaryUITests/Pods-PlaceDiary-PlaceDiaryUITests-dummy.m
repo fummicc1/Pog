d7d181489b008627ce16a0cfca1b855e81d81dba
fummicc1 <fumiya989@gmail.com>
2020-01-28 23:02:03 +0900

    edit gitignore

--- Pods/Target Support Files/Pods-PlaceDiary-PlaceDiaryUITests/Pods-PlaceDiary-PlaceDiaryUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PlaceDiary_PlaceDiaryUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PlaceDiary_PlaceDiaryUITests
-@end
--- Pods/Target Support Files/Pods-PlaceDiary/Pods-PlaceDiary-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PlaceDiary : NSObject
-@end
-@implementation PodsDummy_Pods_PlaceDiary
-@end
