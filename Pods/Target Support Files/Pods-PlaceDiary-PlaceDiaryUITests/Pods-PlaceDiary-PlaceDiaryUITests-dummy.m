ff591e3107982fa58357f30ce259e11abf01f8a3
fummicc1 <fumiya989@gmail.com>
2020-01-28 23:01:05 +0900

    add rxswift. pod

---
+++ Pods/Target Support Files/Pods-PlaceDiary-PlaceDiaryUITests/Pods-PlaceDiary-PlaceDiaryUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PlaceDiary_PlaceDiaryUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PlaceDiary_PlaceDiaryUITests
+@end
---
+++ Pods/Target Support Files/Pods-PlaceDiary/Pods-PlaceDiary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PlaceDiary : NSObject
+@end
+@implementation PodsDummy_Pods_PlaceDiary
+@end
