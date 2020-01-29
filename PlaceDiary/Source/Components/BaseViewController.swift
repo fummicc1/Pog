fb06d607af7255f24ae7d1f6b609f510e9d16070
fummicc1 <fumiya989@gmail.com>
2020-01-30 03:09:05 +0900

    CodeベースでUIを作成していく

--- PlaceDiary/Source/Components/BaseViewController.swift
+++ PlaceDiary/Source/Components/BaseViewController.swift
@@ -8,10 +8,4 @@
 import UIKit
 
 class BaseViewController: UIViewController {
-	override func loadView() {
-		guard let view = R.nib.bottomActionBarViewController(owner: self) else {
-			fatalError()
-		}
-		self.view = view
-	}
 }
--- PlaceDiary/Source/UI/BottomActionBarViewController.swift
+++ PlaceDiary/Source/UI/BottomActionBarViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class BottomActionBarViewController: BaseViewController {
 	
+	private var writeDiaryButton: UIButton = {
+		let button = UIButton()
+		button.translatesAutoresizingMaskIntoConstraints = false
+		return button
+	}()
 	
+	private lazy var stackView: UIStackView = {
+		let stackView = UIStackView(arrangedSubviews: [writeDiaryButton])
+		stackView.translatesAutoresizingMaskIntoConstraints = false
+		return stackView
+	}()
 	
 }
