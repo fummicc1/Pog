040da0f1f4bb29124a1dbe7474c578d1b1b43cd6
fummicc1 <fumiya.tennis1@gmail.com>
2022-07-10 22:04:51 +0900

    Add Observation for GeoFence in `AppDelegate`.

--- iOS/Pog/Pog/PogApp.swift
+++ iOS/Pog/Pog/PogApp.swift
@@ -36,6 +36,7 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
         Analytics.setAnalyticsCollectionEnabled(true)
+        // MARK: Observe location updates
         locationManager.coordinate
             .sink { coordinate in
                 let log = PlaceLog(context: self.store.context)
@@ -43,7 +44,48 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
                 log.lng = coordinate.longitude
                 log.date = Date()
                 log.color = "0x007AFF"
-                try? self.store.context.save()
+                do {
+                    try self.store.context.save()
+                } catch {
+                    assertionFailure("\(error)")
+                }
+            }
+            .store(in: &cancellables)
+        locationManager.onEnterRegion
+            .sink { region in
+                do {
+                    let places = try self.store.fetch(type: InterestingPlace.self)
+                    let visitingLog = InterestingPlaceVisitingLog(
+                        context: self.store.context
+                    )
+                    visitingLog.visitedAt = Date()
+                    guard let place = places.first(
+                        where: { $0.lat == region.center.latitude && $0.lng == region.center.longitude }) else {
+                        assertionFailure()
+                        return
+                    }
+                    visitingLog.place = place
+                    try self.store.context.save()
+                } catch {
+                    assertionFailure("\(error)")
+                }
+            }
+            .store(in: &cancellables)
+        locationManager.onExitRegion
+            .sink { region in
+                do {
+                    let logs = try self.store.fetch(type: InterestingPlaceVisitingLog.self)
+                    guard let log = logs.first(where: {
+                        $0.place?.lat == region.center.latitude && $0.place?.lng == region.center.longitude
+                    }) else {
+                        assertionFailure()
+                        return
+                    }
+                    log.exitedAt = Date()
+                    try self.store.context.save()
+                } catch {
+                    assertionFailure("\(error)")
+                }
             }
             .store(in: &cancellables)
         
