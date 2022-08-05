2d1fc337598cdf4824915a78754f1b2b38d7ca3c
fummicc1 <fumiya.tennis1@gmail.com>
2022-08-05 20:28:40 +0900

    #54 Add SwiftGen

---
+++ iOS/Pog/Pog/Resources/Generated/Strings+Generated.swift
@@ -0,0 +1,172 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
+internal enum L10n {
+  /// About Pog
+  internal static let aboutPog = L10n.tr("Localizable", "About Pog", fallback: #"About Pog"#)
+  /// About User's Data
+  internal static let aboutData = L10n.tr("Localizable", "AboutData", fallback: #"About User's Data"#)
+  /// About User's Location
+  internal static let aboutLocation = L10n.tr("Localizable", "AboutLocation", fallback: #"About User's Location"#)
+  /// About Location tracking feature
+  internal static let aboutLocationTrackingFeature = L10n.tr("Localizable", "AboutLocationTrackingFeature", fallback: #"About Location tracking feature"#)
+  /// About Notification feature
+  internal static let aboutNotificationFeature = L10n.tr("Localizable", "AboutNotificationFeature", fallback: #"About Notification feature"#)
+  /// About Pog（Feature）
+  internal static let aboutPogFeatures = L10n.tr("Localizable", "AboutPogFeatures", fallback: #"About Pog（Feature）"#)
+  /// Accuracy（meter）
+  internal static let accuracyUnitMeter = L10n.tr("Localizable", "Accuracy_unit_meter", fallback: #"Accuracy（meter）"#)
+  /// always
+  internal static let always = L10n.tr("Localizable", "Always", fallback: #"always"#)
+  /// Arrived At
+  internal static let arrivalTime = L10n.tr("Localizable", "ArrivalTime", fallback: #"Arrived At"#)
+  /// Always allow location request
+  internal static let authorizeAlwaysLocationRequest = L10n.tr("Localizable", "AuthorizeAlwaysLocationRequest", fallback: #"Always allow location request"#)
+  /// Cancel
+  internal static let cancel = L10n.tr("Localizable", "Cancel", fallback: #"Cancel"#)
+  /// Close
+  internal static let close = L10n.tr("Localizable", "Close", fallback: #"Close"#)
+  /// confirm next time
+  internal static let confirmNextTime = L10n.tr("Localizable", "ConfirmNextTime", fallback: #"confirm next time"#)
+  /// confirm with app
+  internal static let confirmWithApp = L10n.tr("Localizable", "ConfirmWithApp", fallback: #"confirm with app"#)
+  /// Confirm with Settings app
+  internal static let confirmWithSettings = L10n.tr("Localizable", "ConfirmWithSettings", fallback: #"Confirm with Settings app"#)
+  /// Default
+  internal static let `default` = L10n.tr("Localizable", "Default", fallback: #"Default"#)
+  /// Delete
+  internal static let delete = L10n.tr("Localizable", "Delete", fallback: #"Delete"#)
+  /// Departed At
+  internal static let departureTime = L10n.tr("Localizable", "DepartureTime", fallback: #"Departed At"#)
+  /// Disable Notification
+  internal static let disableNotification = L10n.tr("Localizable", "DisableNotification", fallback: #"Disable Notification"#)
+  /// Display Mode
+  internal static let displayMode = L10n.tr("Localizable", "DisplayMode", fallback: #"Display Mode"#)
+  /// Distance for Notification to be Triggered
+  internal static let distanceToTriggerNotification = L10n.tr("Localizable", "DistanceToTriggerNotification", fallback: #"Distance for Notification to be Triggered"#)
+  /// Do you want to clear the log completely?
+  internal static let doesDeleteAllLogsTotally = L10n.tr("Localizable", "DoesDeleteAllLogsTotally", fallback: #"Do you want to clear the log completely?"#)
+  /// Do you want to completely clear the notification?
+  internal static let doesDeleteAllNotificationTotally = L10n.tr("Localizable", "DoesDeleteAllNotificationTotally", fallback: #"Do you want to completely clear the notification?"#)
+  /// DONE
+  internal static let done = L10n.tr("Localizable", "DONE", fallback: #"DONE"#)
+  /// Enable Notification
+  internal static let enableNotification = L10n.tr("Localizable", "EnableNotification", fallback: #"Enable Notification"#)
+  /// Environment
+  internal static let environment = L10n.tr("Localizable", "Environment", fallback: #"Environment"#)
+  /// Error Occured
+  internal static let errorOccured = L10n.tr("Localizable", "ErrorOccured", fallback: #"Error Occured"#)
+  /// failed to fetch place's name.
+  internal static let failedToFetchPlaceName = L10n.tr("Localizable", "FailedToFetchPlaceName", fallback: #"failed to fetch place's name."#)
+  /// failed to find registered place's name
+  internal static let failedToFetchRegisteredPlaceName = L10n.tr("Localizable", "FailedToFetchRegisteredPlaceName", fallback: #"failed to find registered place's name"#)
+  /// Feedback
+  internal static let feedback = L10n.tr("Localizable", "Feedback", fallback: #"Feedback"#)
+  /// fuzzy location
+  internal static let fuzzyLocation = L10n.tr("Localizable", "FuzzyLocation", fallback: #"fuzzy location"#)
+  /// is near
+  internal static let isNear = L10n.tr("Localizable", "IsNear", fallback: #"is near"#)
+  /// The smaller the number, the more accurate the recording.
+  internal static let lessTheNumberIsMoreAccurateRecordedLocationIs = L10n.tr("Localizable", "Less the number is, more accurate recorded location is.", fallback: #"The smaller the number, the more accurate the recording."#)
+  /// Pog has a feature to keep a log of location. Please allow location request to use these features.
+  internal static let locationAuthorizeRecommendation = L10n.tr("Localizable", "LocationAuthorizeRecommendation", fallback: #"Pog has a feature to keep a log of location. Please allow location request to use these features."#)
+  /// Location Authorized Status
+  internal static let locationAuthorizeStatus = L10n.tr("Localizable", "LocationAuthorizeStatus", fallback: #"Location Authorized Status"#)
+  /// Location Privacy
+  internal static let locationPrivacy = L10n.tr("Localizable", "LocationPrivacy", fallback: #"Location Privacy"#)
+  /// Location request is always allowed
+  internal static let locationRequestIsAlwaysAuthorized = L10n.tr("Localizable", "LocationRequestIsAlwaysAuthorized", fallback: #"Location request is always allowed"#)
+  /// Log
+  internal static let log = L10n.tr("Localizable", "Log", fallback: #"Log"#)
+  /// Map
+  internal static let map = L10n.tr("Localizable", "Map", fallback: #"Map"#)
+  /// If you turn it off, location information will not be recorded if the app is not running.
+  internal static let messageAboutUpdateOnBackground = L10n.tr("Localizable", "MessageAboutUpdateOnBackground", fallback: #"If you turn it off, location information will not be recorded if the app is not running."#)
+  /// deny
+  internal static let notAuthorized = L10n.tr("Localizable", "NotAuthorized", fallback: #"deny"#)
+  /// There is no visiting yet.
+  internal static let noVisitingHistoryYet = L10n.tr("Localizable", "NoVisitingHistoryYet", fallback: #"There is no visiting yet."#)
+  /// Open Settings
+  internal static let openSettings = L10n.tr("Localizable", "OpenSettings", fallback: #"Open Settings"#)
+  /// precise location
+  internal static let preciseLocation = L10n.tr("Localizable", "PreciseLocation", fallback: #"precise location"#)
+  /// registered Place
+  internal static let registeredPlace = L10n.tr("Localizable", "RegisteredPlace", fallback: #"registered Place"#)
+  /// Register Notification
+  internal static let registerNotification = L10n.tr("Localizable", "RegisterNotification", fallback: #"Register Notification"#)
+  /// Review Pog
+  internal static let reviewPog = L10n.tr("Localizable", "ReviewPog", fallback: #"Review Pog"#)
+  /// Save
+  internal static let save = L10n.tr("Localizable", "Save", fallback: #"Save"#)
+  /// Find a Place to Notify
+  internal static let searchPlacesToRegister = L10n.tr("Localizable", "SearchPlacesToRegister", fallback: #"Find a Place to Notify"#)
+  /// Select Date
+  internal static let selectDate = L10n.tr("Localizable", "SelectDate", fallback: #"Select Date"#)
+  /// Totally Delete All Logs
+  internal static let totallyDeleteAllLogs = L10n.tr("Localizable", "TotallyDeleteAllLogs", fallback: #"Totally Delete All Logs"#)
+  /// Totally Delete All Notifications for Places
+  internal static let totallyDeleteAllNotifications = L10n.tr("Localizable", "TotallyDeleteAllNotifications", fallback: #"Totally Delete All Notifications for Places"#)
+  /// To Use Pog confortably
+  internal static let toUseConfortably = L10n.tr("Localizable", "ToUseConfortably", fallback: #"To Use Pog confortably"#)
+  /// Unknown
+  internal static let unknown = L10n.tr("Localizable", "Unknown", fallback: #"Unknown"#)
+  /// Location Update on Background
+  internal static let updateOnBackground = L10n.tr("Localizable", "UpdateOnBackground", fallback: #"Location Update on Background"#)
+  /// Visit
+  internal static let visit = L10n.tr("Localizable", "Visit", fallback: #"Visit"#)
+  /// • Visiting History
+  internal static let visitingHistory = L10n.tr("Localizable", "VisitingHistory", fallback: #"• Visiting History"#)
+  /// • Visiting History（Latest ten）
+  internal static let visitingHistoryTen = L10n.tr("Localizable", "VisitingHistory (Ten)", fallback: #"• Visiting History（Latest ten）"#)
+  /// Visited At
+  internal static let visitingTime = L10n.tr("Localizable", "VisitingTime", fallback: #"Visited At"#)
+  /// What is Pog?
+  internal static let whatIsPog = L10n.tr("Localizable", "WhatIsPog", fallback: #"What is Pog?"#)
+  /// What is Pog（1/3）
+  internal static let whatIsPog13 = L10n.tr("Localizable", "WhatIsPog_1/3", fallback: #"What is Pog（1/3）"#)
+  /// Pog is an application that constantly records the user's location. You will also be notified that you are currently visiting the registered location.
+  internal static let whatIsPog13Headline = L10n.tr("Localizable", "WhatIsPog_1/3_Headline", fallback: #"Pog is an application that constantly records the user's location. You will also be notified that you are currently visiting the registered location."#)
+  /// When using the location tracking feature, it is necessary to set the location request authorization to "Always allow" from the Settings app. Location data is not stored in the cloud, only on the local device.
+  internal static let whatIsPog13Message = L10n.tr("Localizable", "WhatIsPog_1/3_Message", fallback: #"When using the location tracking feature, it is necessary to set the location request authorization to "Always allow" from the Settings app. Location data is not stored in the cloud, only on the local device."#)
+  /// Notification feature（2/3）
+  internal static let whatIsPog23 = L10n.tr("Localizable", "WhatIsPog_2/3", fallback: #"Notification feature（2/3）"#)
+  /// If the location registered from the "Map" screen is within 300 meters from your current location, you can receive a notification from the app.
+  internal static let whatIsPog23Message = L10n.tr("Localizable", "WhatIsPog_2/3_Message", fallback: #"If the location registered from the "Map" screen is within 300 meters from your current location, you can receive a notification from the app."#)
+  /// Location Tracking feature（3/3）
+  internal static let whatIsPog33 = L10n.tr("Localizable", "WhatIsPog_3/3", fallback: #"Location Tracking feature（3/3）"#)
+  /// Introducing a feature that constantly records the user's location. Location tracking is completely activated by setting Pog's location to "Always Allow" from iOS settings. In addition, the log when a certain time elapses or a distance movement occurs is also displayed as a map-pin.
+  internal static let whatIsPog33Message = L10n.tr("Localizable", "WhatIsPog_3/3_Message", fallback: #"Introducing a feature that constantly records the user's location. Location tracking is completely activated by setting Pog's location to "Always Allow" from iOS settings. In addition, the log when a certain time elapses or a distance movement occurs is also displayed as a map-pin."#)
+  /// authorize while app is in use
+  internal static let whileInUse = L10n.tr("Localizable", "WhileInUse", fallback: #"authorize while app is in use"#)
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
