9a4eb87cc3987eadcc4867f67768c2318a9555a3
fummicc1 <fumiya.tennis1@gmail.com>
2022-08-18 12:48:52 +0900

    #60 fixed unittest

--- iOS/Pog/PogTests/Utils/Stubs.swift
+++ iOS/Pog/PogTests/Utils/Stubs.swift
@@ -24,9 +24,9 @@ enum Stubs {
         return ref.addingTimeInterval(TimeInterval(-1 * 60 * 60 * index))
     }
 
-    static var placeLogs: [PlaceLog] {
-        let logs: [PlaceLog] = (0..<10).map { index in
-            let log = PlaceLog()
+    static var placeLogs: [PlaceLogData] {
+        let logs: [PlaceLogData] = (0..<10).map { index in
+            let log = PlaceLogData()
             log.lat = createLat(from: index)
             log.lng = createLng(from: index)
             log.date = createDate(from: index)
@@ -35,9 +35,9 @@ enum Stubs {
         return logs
     }
 
-    static var interestingPlaces: [InterestingPlace] {
-        let places: [InterestingPlace] = (0..<10).map { index in
-            let place = InterestingPlace()
+    static var interestingPlaces: [InterestingPlaceData] {
+        let places: [InterestingPlaceData] = (0..<10).map { index in
+            let place = InterestingPlaceData()
             place.lat = createLat(from: index)
             place.lng = createLng(from: index)
             place.distanceMeter = Double((index + 1) * 10)
